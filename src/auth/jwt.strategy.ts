72459782c29356b6bbf0546717f79a0dc69db864
santiagoalomoto <sic.alomoto@yavirac.edu.ec>
2025-06-19 23:08:05 -0500

    arreglo de ID de mongo DB

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,4 +1,3 @@
-// jwt.strategy.ts
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
@@ -8,11 +7,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET || 'miClaveSecretaMuySegura', // misma clave que en auth.module.ts
+      secretOrKey: process.env.JWT_SECRET || 'miClaveSecretaMuySegura',
     });
   }
 
   async validate(payload: any) {
-    return payload; // id, email, role
+    return {
+      id: payload.id || payload.sub, // asegura que req.user.id esté disponible
+      email: payload.email,
+      role: payload.role,
+    };
   }
 }
+
